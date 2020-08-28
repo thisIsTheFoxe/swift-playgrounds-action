8ef55f9566aeb50633233b0e8bd928407beb2e87
Henrik Storch <henrik_storch@hugoboss.com>
2020-08-28 09:41:42 +0200

    fix action fail with non-0 exitcode

--- pg.sh
+++ pg.sh
@@ -1,17 +1,35 @@
-HL='\e[0;33m' # Highlight
-NC='\e[0m' # No Color
+HL='\e[33m'		# Highlight
+RED='\e[31m'		# Red
+GREEN='\e[32m'		# Green
+CHECK='\xE2\x9C\x94'	# Checkmark
+NC='\e[0m'		# No Color
 
+exitCode=0;
 
 for file in *.playground; do 
   cd $file; 
   cp Contents.swift main.swift;
 
   printf "${HL}Compiling $file${NC}\n";
-  [ -e Sources/*.swift ] && swiftc -v main.swift Sources/*.swift || swiftc -v main.swift;
- 
+  if !([ -e Sources/*.swift ] && swiftc -v main.swift Sources/*.swift || swiftc -v main.swift);
+  then
+    printf "${RED}Failed to compile $file${NC}\n";
+    exitCode=1;
+    rm main.swift;
+    cd ..;
+    continue;
+  fi;
   printf "${HL}Running $file${NC}\n";
-  ./main;
+  if !(./main);
+  then
+    printf "${RED}Failed to run $file${NC}\n";
+    exitCode=1;
+  else
+    printf "${GREEN}${CHECK} $file${NC}\n";
+  fi;
   rm main;
   rm main.swift;
-  cd ..; 
+  cd ..;
 done;
+
+exit $exitCode;
